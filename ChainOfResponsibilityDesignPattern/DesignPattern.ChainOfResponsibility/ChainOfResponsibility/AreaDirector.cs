d9030dd5270111914bdf18be12b7552fc3442875
maliiayik <maliiayik@gmail.com>
2024-03-03 12:33:31 +0300

    Add project files.

---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/ChainOfResponsibility/AreaDirector.cs
@@ -0,0 +1,34 @@
+﻿using DesignPattern.ChainOfResponsibility.DAL;
+using DesignPattern.ChainOfResponsibility.Models;
+
+namespace DesignPattern.ChainOfResponsibility.ChainOfResponsibility
+{
+    public class AreaDirector : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            Context context = new Context();
+
+            if (req.Amount <= 400000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Name = req.Name;
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.EmployeeName = "Bölge Direktörü - Özge ÖZTÜRK";
+                customerProcess.Description = "Para Çekme İşlemi Onaylandı, Müşteriye Talep Ettiği Tutar Ödendi";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount.ToString();
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Direktörü - Özge ÖZTÜRK";
+                customerProcess.Description = "Para Çekme Tutarı Bölge Direktörünün Günlük Ödeyebileceği Limiti Aştığı İçin İşlem Gerçekleştirilemedi, Müşterinin Günlük Maksimum Çekebileceği Tutar 400.000₺ olup daha fazlası için birden fazla gün şubeye gelmesi gerekmektedir.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();                
+            }
+        }
+    }
+}
