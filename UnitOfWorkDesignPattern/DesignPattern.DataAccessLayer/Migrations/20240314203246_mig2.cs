cc5c4e15bded57a77cdd599076132fa892af6299
maliiayik <maliiayik@gmail.com>
2024-03-18 21:00:59 +0300

    UnitOfWork

---
+++ UnitOfWorkDesignPattern/DesignPattern.DataAccessLayer/Migrations/20240314203246_mig2.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesignPattern.DataAccessLayer.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "NewReceiverBalance",
+                table: "Customers",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "NewSenderBalance",
+                table: "Customers",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NewReceiverBalance",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "NewSenderBalance",
+                table: "Customers");
+        }
+    }
+}
---
+++ UnitOfWorkDesignPattern/Designpattern.UnitOfWork/Controllers/DefaultController.cs
@@ -0,0 +1,58 @@
+﻿using Designpattern.UnitOfWork.Models;
+using DesignPattern.BusinessLayer.Abstract;
+using DesignPattern.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Designpattern.UnitOfWork.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICustomerService _customerService;
+
+        public DefaultController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {               
+            return View();
+        }
+
+        /// <summary>
+        /// Alıcı ve gönderici değerlerini modelden gelen değerleri update ediyoruz.
+        /// </summary>       
+
+        [HttpPost]
+        public IActionResult Index(CustomerViewModel model)
+        {
+            // Gönderen ve alıcı müşteri bilgilerini al
+            var sender = _customerService.TGetByID(model.SenderID);
+            var receiver = _customerService.TGetByID(model.ReceiverID);
+
+
+            // Gönderenin bakiyesini güncelle
+            sender.CustomerBalance -= model.Amount;
+            // Alıcının bakiyesini güncelle
+            receiver.CustomerBalance += model.Amount;
+
+            // Güncellenmiş müşteri listesini oluştur
+            List<Customer> modifiedCustomers = new List<Customer>()
+            {
+                  sender,
+                 receiver
+             };
+
+
+            // Müşteri bilgilerini güncelle
+            _customerService.TMultiUpdate(modifiedCustomers);
+
+            // View'e yönlendir
+            return View();
+        }
+
+    }
+}
